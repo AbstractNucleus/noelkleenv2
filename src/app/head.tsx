662ce1c5279c3366d39e031ea3d39e197630f702
AbstractNucleus <noelhkleen@gmail.com>
2023-02-03 19:34:13 +0100

    init

---
+++ src/app/head.tsx
@@ -0,0 +1,16 @@
+export default function Head() {
+    return (
+        <>
+            <title>Noel Kleen</title>
+            <meta
+                content="width=device-width, initial-scale=1"
+                name="viewport"
+            />
+            <meta
+                name="description"
+                content="My personal website for navigating my projects"
+            />
+            <link rel="icon" href="/favicon.ico" />
+        </>
+    );
+}
